974a13e5304ffb5aa023509394ffe1e32d34aab4
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-02-25 20:59:40 +0530

    update promotions.php

--- promotions.php
+++ promotions.php
@@ -23,8 +23,8 @@
 
                     <div class="p-4 mt-4 pt-5">
                         <h2 class="text-3xl font-bold">Promotions</h2>
-                        <div class="bg-[#E7B94E] my-3 h-1 w-[20%] d-none d-lg-block ml-[2%] rounded"></div>
-                        <div class="bg-[#E7B94E] my-2 h-[2px] w-[60%] d-block d-lg-none ml-[2%] rounded"></div>
+                        <div class="bg-[#AD1212] my-3 h-1 w-[20%] d-none d-lg-block ml-[2%] rounded"></div>
+                        <div class="bg-[#AD1212] my-2 h-[2px] w-[60%] d-block d-lg-none ml-[2%] rounded"></div>
                     </div>
 
 
