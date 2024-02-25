b1d3964aa369d06c9fbaa009dbabfba7c95e7193
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-02-25 20:59:48 +0530

    create register.php

---
+++ register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/plugin/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+
+    <?php include "App/includes/header.php"; ?>
+
+    <div class="container-fluid mb-5">
+        <div class="row">
+
+            <div class="col-12 bg-cover mt-3 mt-lg-0 bg-center bg-no-repeat h-[35rem] items-center flex" style="background-image: url('assets/img/acoustic-guitar-snare-drum-black-background-isolated');">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row">
+                            sd
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    </div>
+
+    <?php include "App/includes/footer.php"; ?>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
