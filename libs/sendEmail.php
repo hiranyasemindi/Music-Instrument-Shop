2f17a0c91f5bd8d094fd8d8fcf5574dd6fd309dc
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-03 03:48:38 +0530

    create sendEmail.php

---
+++ libs/sendEmail.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email
+{
+
+    static function sendEmail($email, $body)
+    {
+
+        $mail = new PHPMailer;
+        $mail->IsSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'hiranyagunawardhane@gmail.com';
+        $mail->Password = 'dchmdrjebuiykvqh';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+        $mail->setFrom('hiranyagunawardhane@gmail.com', 'Reset Password');
+        $mail->addReplyTo('hiranyagunawardhane@gmail.com', 'Reset Password');
+        $mail->addAddress($email);
+        $mail->isHTML(true);
+        $mail->Subject = 'Sonority Forgot Password Verification Code';
+        $bodyContent = $body;
+        $mail->Body    = $bodyContent;
+
+
+        if (!$mail->send()) {
+            return 'Verification code sending failed';
+        } else {
+            return "Success";
+        }
+    }
+}
