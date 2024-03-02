fb5b06ffb4c2d7dc1ea852b71a9421a50143dfb6
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-03 03:48:06 +0530

    update register.php

--- register.php
+++ register.php
@@ -17,12 +17,11 @@
 
             <div class="col-10">
                 <div class="row">
-                    <div class="col-lg-6 col-12 lg:h-[550px] sm:h-[150px] bg-black">
+                    <div class="col-lg-6 col-12 lg:h-[690px] sm:h-[150px] bg-black">
                         <div class="row ">
-                            <div class="col-10 offset-1">
+                            <div class="col-10 offset-1 mt-5">
                                 <div class="row">
-                                    <img src="logo/logoo.png" alt="" width="100px" height="100px">
-
+                                    <img src="logo/logoo.png" alt="" width="200px" height="100px">
                                 </div>
                             </div>
                         </div>
@@ -54,8 +53,15 @@
                                     }
                                     ?>
                                     <input type="email" class="border h-[40%] p-3 mt-3 focus:outline-none mt-3" id="loginEmail" placeholder="Email" value="<?php echo $email; ?>">
-                                    <input type="password" class="border h-[40%] p-3 mt-3 focus:outline-none" id="loginPassword" placeholder="Password" value="<?php echo $password; ?>">
-                                    <p class=" text-end pt-1 text-[#AD1212] mt-3 mt-lg-0">Forgot Password?</p>
+                                    <div class="col-12   ">
+                                        <div class="row  ">
+                                            <input type="password" class="border-y border-l w-[85%]  h-[40%]  py-3 ps-3 focus:outline-none mt-3" id="loginPassword" placeholder="Password" value="<?php echo $password; ?>">
+                                            <div class="w-[15%] border-y border-r flex items-center justify-end  mt-3">
+                                                <i class="bi bi-eye-slash-fill hover:cursor-pointer text-xl  text-[#A9A9AF] " id="icon-visibility" onclick="loginVisibility();"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class=" text-end pt-1 text-[#AD1212] mt-3 hover:cursor-pointer mt-lg-0" onclick="forgotPassowrd();">Forgot Password?</p>
                                     <div class="flex items-center mt-3 mt-lg-0">
                                         <input class="cyberpunk-checkbox" type="checkbox" name="" id="rememberMe">
                                         <span class="ms-2  ">Remember Me</span>
@@ -65,6 +71,63 @@
                                 </div>
                             </div>
                             <!-- signinbox -->
+
+                            <!-- model -->
+
+
+                            <!-- Modal toggle -->
+                            <!-- <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                                Toggle modal
+                            </button> -->
+
+                            <!-- Main modal -->
+                            <div id="fp-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 mt-[8%] ml-[35%] h-[calc(100%-1rem)] max-h-full">
+                                <div class="relative p-4 w-full max-w-md max-h-full">
+                                    <!-- Modal content -->
+                                    <div class="relative bg-white rounded-lg shadow ">
+                                        <!-- Modal header -->
+                                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
+                                            <h3 class="text-xl font-semibold text-gray-900">
+                                                Reset Your Passsword
+                                            </h3>
+                                            <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="fp-modal">
+                                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                </svg>
+                                                <span class="sr-only">Close modal</span>
+                                            </button>
+                                        </div>
+                                        <!-- Modal body -->
+                                        <div class="p-4 md:p-5">
+                                            <form class="space-y-4" action="#">
+                                                <div id="fp_alertBox" class="flex items-center p-4 d-none mb-2 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50" role="alert">
+                                                    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                                                    </svg>
+                                                    <span class="sr-only">Info</span>
+                                                    <div id="fp_alert">
+                                                        <span class="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <label class="block mb-2 text-sm font-medium text-gray-900 ">Enter the verification Code</label>
+                                                    <input type="text" id="vcode" placeholder="****" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " required />
+                                                </div>
+                                                <div>
+                                                    <label class="block mb-2 text-sm font-medium text-gray-900 ">New password</label>
+                                                    <input type="password" oninput="validateNewPassword();" id="new_password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " required />
+                                                </div>
+                                                <div>
+                                                    <label class="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label>
+                                                    <input type="password" id="confirm_password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " required />
+                                                </div>
+                                                <button onclick="resetPassword();" type="submit" class="w-full mt-4 text-white bg-[#AD1212] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Confirm</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- model -->
 
                             <!-- signupbox -->
                             <div class="col-10 offset-1 d-none py-5" id="signUpBox">
@@ -90,8 +153,22 @@
                                         </div>
                                     </div>
                                     <input type="text" class="border h-[40%] p-3 mt-3 focus:outline-none" id="email" placeholder="Email">
-                                    <input oninput="validatePassword();" type="password" class="border h-[40%] p-3 mt-3 focus:outline-none" id="password" placeholder="Password">
-                                    <input type="password" class="border h-[40%] p-3 mt-3 focus:outline-none" id="confirmPassword" placeholder="Confirm Password">
+                                    <div class="col-12   ">
+                                        <div class="row  ">
+                                            <input oninput="validatePassword();" type="password" class="border-y border-l w-[85%]  h-[40%]  py-3 ps-3 focus:outline-none mt-3" id="password" placeholder="Password">
+                                            <div class="w-[15%] border-y border-r flex items-center justify-end  mt-3">
+                                                <i class="bi bi-eye-slash-fill hover:cursor-pointer text-xl  text-[#A9A9AF] " id="icon-visibility2" onclick="registerVisibility();"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12   ">
+                                        <div class="row  ">
+                                            <input oninput="validatePassword();" type="password" class="border-y border-l w-[85%]  h-[40%]  py-3 ps-3 focus:outline-none mt-3" id="confirmPassword" placeholder="Confirm Password">
+                                            <div class="w-[15%] border-y border-r flex items-center justify-end  mt-3">
+                                                <i class="bi bi-eye-slash-fill hover:cursor-pointer text-xl  text-[#A9A9AF] " id="icon-visibility3" onclick="registerConfirmVisibility();"></i>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div class="w-[49%] me-[2%]">
                                         <div class="row ">
                                             <input type="text" class="border h-[40%] p-3 mt-3 focus:outline-none mt-3" id="mobile" placeholder="Mobile">
@@ -134,6 +211,7 @@
 
 
     <script src="assets/js/script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </body>
 
 </html>
