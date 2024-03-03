198beeef8fef90b13e999c52f5072a3862fcd011
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-04 03:30:04 +0530

    update register.php

--- register.php
+++ register.php
@@ -72,14 +72,6 @@
                             </div>
                             <!-- signinbox -->
 
-                            <!-- model -->
-
-
-                            <!-- Modal toggle -->
-                            <!-- <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                                Toggle modal
-                            </button> -->
-
                             <!-- Main modal -->
                             <div id="fp-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 mt-[8%] ml-[35%] h-[calc(100%-1rem)] max-h-full">
                                 <div class="relative p-4 w-full max-w-md max-h-full">
@@ -127,7 +119,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <!-- model -->
+                            <!-- Main modal -->
 
                             <!-- signupbox -->
                             <div class="col-10 offset-1 d-none py-5" id="signUpBox">
@@ -163,7 +155,7 @@
                                     </div>
                                     <div class="col-12   ">
                                         <div class="row  ">
-                                            <input  type="password" class="border-y border-l w-[85%]  h-[40%]  py-3 ps-3 focus:outline-none mt-3" id="confirmPassword" placeholder="Confirm Password">
+                                            <input type="password" class="border-y border-l w-[85%]  h-[40%]  py-3 ps-3 focus:outline-none mt-3" id="confirmPassword" placeholder="Confirm Password">
                                             <div class="w-[15%] border-y border-r flex items-center justify-end  mt-3">
                                                 <i class="bi bi-eye-slash-fill hover:cursor-pointer text-xl  text-[#A9A9AF] " id="icon-visibility3" onclick="registerConfirmVisibility();"></i>
                                             </div>
@@ -199,14 +191,11 @@
                                 </div>
                             </div>
                             <!-- signupbox -->
-
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
     </div>
 
 
