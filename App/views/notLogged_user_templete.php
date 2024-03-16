bc8bece461538b823548b507235579060200bf8a
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-16 23:09:16 +0530

    create loggeduser

---
+++ App/views/notLogged_user_templete.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/plugin/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <?php
+    include "App/includes/header.php"; ?>
+    <span>Please login first</span>
+    <?php include "App/includes/footer.php"; ?>
+
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
