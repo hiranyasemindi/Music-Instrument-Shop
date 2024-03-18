ad93499989e4f6dddf6ade26bb04500fbb6b9658
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-19 02:09:01 +0530

    update customerreviews

--- App/includes/customerRreviews.php
+++ App/includes/customerRreviews.php
@@ -42,43 +42,52 @@
             }
         }
     </style>
-
-    <div class="text-center col-12 ">
-        <div class="row align-items-center">
-            <div class="col-1 text-end d-none d-lg-block">
-                <i class="bi bi-caret-left-fill text-3xl hover:pointer" id="previous-customerreviews"></i>
-            </div>
-            <div class="relative h-[40%] col-lg-10 col-12" id="cover-customerreviews">
-                <div class="scroll-images-customerreviews text-center ">
-                    <?php
-                    for ($x = 0; $x < 10; $x++) {
-                    ?>
-                        <div class="col-lg-4 col-12">
-                            <div class="row">
-                                <div class="col-10 offset-1 shadow text-center">
-                                    <div class="flex mb-2 items-center justify-center mt-4">
-                                        <img class="rounded-circle" width="50px"  src="assets/img/profile.jpg" alt="">
-                                    </div>
-                                    <span class="font-bold text-lg">Marshell Broo</span>
-                                    <div class="px-4">
-                                        <p class="text-md py-4 text-[#999b9e]">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-                                        </p>
+    <?php
+    class CutomerReviewsTemplete
+    {
+        public static function generate($customerReviews)
+        {
+    ?>
+            <div class="text-center col-12 ">
+                <div class="row align-items-center">
+                    <div class="col-1 text-end d-none d-lg-block">
+                        <i class="bi bi-caret-left-fill text-3xl hover:pointer" id="previous-customerreviews"></i>
+                    </div>
+                    <div class="relative h-[40%] col-lg-10 col-12" id="cover-customerreviews">
+                        <div class="scroll-images-customerreviews text-center ">
+                            <?php
+                            while ($review = $customerReviews->fetch_assoc()) {
+                            ?>
+                                <div class="col-lg-4 col-12">
+                                    <div class="row">
+                                        <div class="col-10 offset-1 shadow text-center">
+                                            <div class="flex mb-2 items-center justify-center mt-4">
+                                                <img class="rounded-circle" width="50px" src="<?php echo $review["profile_img"] ? $review["profile_img"] : "assets/img/round_profil_picture_before_.png";  ?>" alt="">
+                                            </div>
+                                            <span class="font-bold text-lg"><?php echo $review["fname"] . " " . $review["lname"]; ?></span>
+                                            <div class="px-4">
+                                                <p class="text-md py-4 text-[#999b9e]">
+                                                    "<?php echo $review["review"];  ?>"
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            <?php
+                            }
+                            ?>
                         </div>
-                    <?php
-                    }
-                    ?>
+                    </div>
+                    <div class="col-1 text-start d-none d-lg-block">
+                        <i class="bi bi-caret-right-fill text-3xl" id="next-customerreviews" class="arrow"></i>
+                    </div>
                 </div>
             </div>
-            <div class="col-1 text-start d-none d-lg-block">
-                <i class="bi bi-caret-right-fill text-3xl" id="next-customerreviews" class="arrow"></i>
-            </div>
-        </div>
-    </div>
+    <?php
+        }
+    }
+    ?>
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="assets/js/script.js"></script>
