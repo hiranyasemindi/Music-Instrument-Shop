74a33e5a9e1f98fe539a8760d374998dbebbc961
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-24 02:34:38 +0530

    create 404.php

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/plugin/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+
+    <div class="flex items-center justify-center">
+        <img src="assets/img/undraw_page_not_found_re_e9o6.svg" alt="loginImg" width="500px" height="500px">
+    </div>
+    <div class="flex items-center justify-center mt-4">
+        <span class="fw-semibold text-3xl">Not Found</span>
+    </div>
+    <div class="flex items-center justify-center mt-4">
+        <button onclick="window.location.href = 'index.php'" class="bg-[#AD1212] rounded px-5 py-[12px] mt-1 my-5 text-white font-bold">Go to Home</button>
+    </div>
+
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
