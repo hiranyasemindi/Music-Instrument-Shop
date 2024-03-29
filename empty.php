241625e4682de1487ae93f349828b8c37515d68c
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-30 02:41:54 +0530

    create empty.php

---
+++ empty.php
@@ -0,0 +1,21 @@
+<?php
+class EmptyDesign
+{
+    public static function generate($text)
+    {
+?>
+        <div class="col-12 ">
+            <div class="flex items-center justify-center">
+                <img src="assets/img/undraw_no_data_re_kwbl.svg" alt="loginImg" width="500px" height="500px">
+            </div>
+            <div class="flex items-center justify-center mt-4">
+                <span class="fw-semibold text-3xl"><?php echo $text; ?></span>
+            </div>
+            <div class="flex items-center justify-center mt-4">
+                <button onclick="window.location.href = 'index.php'" class="bg-[#AD1212] rounded px-5 py-[12px] mt-1 my-5 text-white font-bold">Go to Home</button>
+            </div>
+        </div>
+<?php
+    }
+}
+?>
