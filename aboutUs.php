8eabdc869c7cf69a8a3b0ca70aa3ef0f4c9396fd
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-03-30 02:40:24 +0530

    update aboutUs.php

--- aboutUs.php
+++ aboutUs.php
@@ -33,7 +33,7 @@
                                 <img src="assets/img/image 21.png" alt="" width="800px" height="600px">
                             </div>
                             <div class="col-lg-4 col-12 ms-lg-3 ms-0">
-                                <img src="assets/img/image 22.png"  class="mt-4 mt-lg-0 pt-3 pt-lg-0" alt="" width="400px" height="300px">
+                                <img src="assets/img/image 22.png" class="mt-4 mt-lg-0 pt-3 pt-lg-0" alt="" width="400px" height="300px">
                                 <img src="assets/img/image 23.png" class="mt-4 pt-3" width="400px" height="300px">
                             </div>
                         </div>
@@ -42,19 +42,23 @@
                     <div class="col-12">
                         <div class="row">
                             <p class="pt-3 mt-4 text-center font-semibold text-3xl">Who We Are</p>
-                            <p class="text-lg p-5 text-[#999b9e]">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                officia deserunt mollit anim id est laborum.</p>
+                            <p class="text-lg p-5 text-[#999b9e]">Welcome to Sonority Music Shop, where passion for music meets exceptional craftsmanship.
+                                As avid musicians ourselves, we understand the profound connection between an artist and their instrument. At Sonority Music Shop,
+                                we curate a diverse range of high-quality musical instruments, catering to both seasoned professionals and aspiring enthusiasts alike.
+                                </br></br>
+                                Our commitment to excellence is evident in every aspect of our establishment. From the moment you step through our doors, you'll be greeted
+                                by a team of knowledgeable professionals eager to assist you on your musical journey. Whether you're in search of a classic acoustic guitar,
+                                a sleek electric keyboard, or a specialty percussion instrument, we have an extensive selection to suit your needs.
+                                </br></br>
+                                Not only do we offer a wide array of instruments, but we also provide personalized guidance to ensure you find the perfect match for your
+                                unique style and preferences. Our staff members are not just salespeople; they're fellow musicians who share your passion and are dedicated
+                                to helping you find your musical voice.
+                                In addition to our impressive inventory, we also offer repair and maintenance services to keep your instruments in optimal condition.
+                                Our skilled technicians utilize state-of-the-art equipment and techniques to ensure your instrument performs flawlessly, allowing you to
+                                focus on what matters most—creating beautiful music.
+                                </br></br>
+                                Whether you're a seasoned professional or a beginner taking your first steps in the world of music, Sonority Music Shop is your destination
+                                for exceptional instruments and unparalleled service. Come visit us today and let us help you make your musical dreams a reality.</p>
                         </div>
                     </div>
 
@@ -68,22 +72,21 @@
                             </div>
 
                             <p class="text-center mt-5 font-semibold text-xl">Vision</p>
-                            <p class="text-lg px-5 mt-3 text-[#999b9e]">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                officia deserunt mollit anim id est laborum.</p>
+                            <div class="col-10 offset-1">
+                                <p class="text-lg px-5 mt-3 text-[#999b9e]">"To be the premier destination for musicians of all levels,
+                                    providing a diverse range of high-quality instruments and unparalleled customer service that inspires creativity,
+                                    fosters community, and enriches lives through the power of music."</p>
+                            </div>
 
                             <p class="text-center mt-5 font-semibold text-xl">Mission</p>
-                            <p class="text-lg px-5 mt-3 text-[#999b9e]">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                officia deserunt mollit anim id est laborum.</p>
+                            <div class="col-10 offset-1">
+                                <p class="text-lg px-5 mt-3 text-[#999b9e]">"At [Shop Name], our mission is to empower musicians by offering a curated
+                                    selection of top-tier instruments, personalized guidance, and expert services. We strive to create a welcoming
+                                    environment where musicians of all backgrounds and skill levels feel valued and supported on their musical
+                                    journey. Through our commitment to excellence, integrity, and passion for music, we aim to exceed expectations,
+                                    build lasting relationships, and spread the joy of music to individuals and communities alike."</p>
+                            </div>
+
                         </div>
                     </div>
 
@@ -92,20 +95,39 @@
                             <p class="text-center mt-5 pt-5  font-semibold text-3xl">Our Management</p>
                             <div class="col-10 offset-1">
                                 <div class="row">
-                                    <?php
-                                    for ($x = 0; $x < 4; $x++) {
-                                    ?>
-                                        <div class="col-lg-3 col-12 mt-5">
-                                            <div class="row flex items-center justify-center">
-                                                <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/round_profil_picture_before_.webp');">
-                                                </div>
-                                                <p class="text-center mt-2 font-semibold">Robert Fox</p>
-                                                <p class="text-center text-[#999b9e]">Director</p>
+
+                                    <div class="col-lg-3 col-12 mt-5">
+                                        <div class="row flex items-center justify-center">
+                                            <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets//img/p1.jpg');">
                                             </div>
+                                            <p class="text-center mt-2 font-semibold">Robert Fox</p>
+                                            <p class="text-center text-[#999b9e]">Director</p>
                                         </div>
-                                    <?php
-                                    }
-                                    ?>
+                                    </div>
+                                    <div class="col-lg-3 col-12 mt-5">
+                                        <div class="row flex items-center justify-center">
+                                            <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p2.jpg');">
+                                            </div>
+                                            <p class="text-center mt-2 font-semibold">Jennie Bath</p>
+                                            <p class="text-center text-[#999b9e]">Director</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-12 mt-5">
+                                        <div class="row flex items-center justify-center">
+                                            <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p3.jpg');">
+                                            </div>
+                                            <p class="text-center mt-2 font-semibold">Anne Dec</p>
+                                            <p class="text-center text-[#999b9e]">Director</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-12 mt-5">
+                                        <div class="row flex items-center justify-center">
+                                            <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p4.jpg');">
+                                            </div>
+                                            <p class="text-center mt-2 font-semibold">Sandrew Mathew</p>
+                                            <p class="text-center text-[#999b9e]">Director</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -114,13 +136,24 @@
                     <div class="col-12">
                         <div class="row">
                             <p class="text-center mt-5 pt-5 font-semibold text-3xl">History of Us</p>
-                            <p class="text-lg px-5 mt-3 text-[#999b9e]">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                officia deserunt mollit anim id est laborum.</p>
+                            <p class="text-lg px-5 mt-3 text-[#999b9e]">"Established in 2001, Sonority Music Shop has been a cornerstone of the music community, proudly serving musicians
+                                and enthusiasts alike for 23 years. Our journey began with a simple yet powerful vision: to create
+                                a haven where musicians could find exceptional instruments, expert guidance, and a supportive community to nurture
+                                their passion for music.
+                                <br><br>
+                                From our humble beginnings, we have grown into a trusted institution renowned for our commitment to excellence
+                                and unwavering dedication to our customers. Over the years, we've evolved alongside the ever-changing landscape
+                                of music, adapting to new technologies and trends while staying true to our core values.
+                                <br><br>
+                                Through hard work, perseverance, and a deep love for music, we've cultivated a reputation for excellence that
+                                extends far beyond our storefront. Our team of passionate musicians and knowledgeable experts are not just here
+                                to sell instruments; we're here to share our expertise, foster creativity, and inspire the next generation of
+                                musicians.
+                                <br><br>
+                                As we reflect on our history, we're grateful for the countless musicians who have trusted us with their musical
+                                journeys. Whether you're a seasoned professional or just starting out, we invite you to be a part of our story
+                                and experience the magic of Sonority Music Shop."
+                            </p>
                         </div>
                     </div>
 
