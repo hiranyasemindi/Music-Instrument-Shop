93ab31f268f701676333bc03eaf9ef6c3fd3189f
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-04-08 23:45:25 +0530

    update productTemplete

--- App/views/productsTemplete.php
+++ App/views/productsTemplete.php
@@ -294,69 +294,73 @@ class DisplayProductsTemplete
         if (!empty($products)) {
 ?>
             <!-- products -->
-            <div class="col-lg-9 col-12" id="products-area">
+            <div class="col-9 ">
                 <div class="row">
-                    <?php
-                    while ($product = $products->fetch_assoc()) {
-                    ?>
-                        <div class="col-6 col-lg-3 mb-5">
-                            <div class="row">
-                                <div class="col-10 offset-1  card">
+                    <div class=" col-12" id="products-area">
+                        <div class="row">
+                            <?php
+                            while ($product = $products->fetch_assoc()) {
+                            ?>
+                                <div class="col-6 col-lg-3 mb-5">
+                                    <div class="row">
+                                        <div class="col-10 offset-1  card">
 
-                                    <div class="flex items-center justify-center">
-                                        <img src="<?php echo $product["image_path"]; ?>" alt="product_img" width="200px" height="200px">
-                                    </div>
-
-                                    <div class="row product-onclick-view justify-content-center align-content-center " style="position: absolute;" id="hover-view">
-                                        <div style="width: 100%;">
-                                            <div class="col-12">
-
+                                            <div class="flex items-center justify-center">
+                                                <img src="<?php echo $product["image_path"]; ?>" alt="product_img" width="200px" height="200px">
                                             </div>
-                                        </div>
 
-                                        <div style="width: 100%;" class="mt-3 text-center  align-items-center">
-                                            <div class="product-dot col-12 ">
-                                                <div class="d-flex align-items-center justify-content-center" style="height: 100%;">
-                                                    <div onclick="window.location.href = 'singleProductView?id=<?php echo $product['id']; ?>'" class="col-2 d-flex justify-content-center align-items-center" style="color: #AD1212; border-radius: 50%; width: 2.5rem; height: 2.5rem; background-color: #fcb3b3;">
-                                                        <i class="bi bi-eye fs-5"></i>
+                                            <div class="row product-onclick-view justify-content-center align-content-center " style="position: absolute;" id="hover-view">
+                                                <div style="width: 100%;">
+                                                    <div class="col-12">
+
                                                     </div>
-                                                    <div class="col-1"></div>
-                                                    <div onclick="addToWishlist(<?php echo $product['id']; ?>);" class="col-2 d-flex justify-content-center align-items-center" style="color: #AD1212; border-radius: 50%; width: 2.5rem; height: 2.5rem; background-color: #fcb3b3;">
-                                                        <i class="bi bi-heart fs-5 mt-1"></i>
-                                                    </div>
-                                                    <div class="col-1"></div>
-                                                    <div onclick="addToCart(<?php echo $product['id']; ?>);" class="col-2 d-flex justify-content-center align-items-center" style="color: #AD1212; border-radius: 50%; width: 2.5rem; height: 2.5rem; background-color: #fcb3b3;">
-                                                        <i class="bi bi-bag fs-5"></i>
+                                                </div>
+
+                                                <div style="width: 100%;" class="mt-3 text-center  align-items-center">
+                                                    <div class="product-dot col-12 ">
+                                                        <div class="d-flex align-items-center justify-content-center" style="height: 100%;">
+                                                            <div onclick="window.location.href = 'singleProductView?id=<?php echo $product['id']; ?>'" class="col-2 d-flex justify-content-center align-items-center" style="color: #AD1212; border-radius: 50%; width: 2.5rem; height: 2.5rem; background-color: #fcb3b3;">
+                                                                <i class="bi bi-eye fs-5"></i>
+                                                            </div>
+                                                            <div class="col-1"></div>
+                                                            <div onclick="addToWishlist(<?php echo $product['id']; ?>);" class="col-2 d-flex justify-content-center align-items-center" style="color: #AD1212; border-radius: 50%; width: 2.5rem; height: 2.5rem; background-color: #fcb3b3;">
+                                                                <i class="bi bi-heart fs-5 mt-1"></i>
+                                                            </div>
+                                                            <div class="col-1"></div>
+                                                            <div onclick="addToCart(<?php echo $product['id']; ?>);" class="col-2 d-flex justify-content-center align-items-center" style="color: #AD1212; border-radius: 50%; width: 2.5rem; height: 2.5rem; background-color: #fcb3b3;">
+                                                                <i class="bi bi-bag fs-5"></i>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="p-3 text-center col-10 offset-1 h-[180px] border shadow-sm justify-center">
-                                    <p class="text-md "><?php echo $product["title"]; ?></p>
-                                    <p class="text-lg mt-2 fw-semibold">Rs.<?php echo $product["price"]; ?>.00</p>
-                                    <div class=" mt-2">
-                                        <span class="col-2 text-center text-lg-end fw-bold" style="color: #AD1212;"><?php echo $product["rating"]; ?>.0</span>
+                                        <div class="p-3 text-center col-10 offset-1 h-[180px] border shadow-sm justify-center">
+                                            <p class="text-md "><?php echo $product["title"]; ?></p>
+                                            <p class="text-lg mt-2 fw-semibold">Rs.<?php echo $product["price"]; ?>.00</p>
+                                            <div class=" mt-2">
+                                                <span class="col-2 text-center text-lg-end fw-bold" style="color: #AD1212;"><?php echo $product["rating"]; ?>.0</span>
 
-                                        <span class="col-6">
-                                            <?php
-                                            $fill = $product["rating"];
-                                            for ($x = 0; $x < 5; $x++) {
-                                                $starClass = ($x < $fill) ? "bi bi-star-fill" : "bi bi-star";
-                                            ?>
-                                                <i class="<?php echo $starClass; ?> p-1" style="color: #AD1212;"></i>
-                                            <?php
-                                            }
-                                            ?>
-                                        </span>
+                                                <span class="col-6">
+                                                    <?php
+                                                    $fill = $product["rating"];
+                                                    for ($x = 0; $x < 5; $x++) {
+                                                        $starClass = ($x < $fill) ? "bi bi-star-fill" : "bi bi-star";
+                                                    ?>
+                                                        <i class="<?php echo $starClass; ?> p-1" style="color: #AD1212;"></i>
+                                                    <?php
+                                                    }
+                                                    ?>
+                                                </span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            <?php
+                            }
+                            ?>
                         </div>
-                    <?php
-                    }
-                    ?>
+                    </div>
                 </div>
             </div>
             <!-- products -->
