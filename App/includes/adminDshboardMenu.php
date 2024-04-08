d09480b5b24d4e0a500d3791d4e4988d6622c350
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-04-08 23:44:14 +0530

    update adminDashboardMenu.php

--- App/includes/adminDshboardMenu.php
+++ App/includes/adminDshboardMenu.php
@@ -1,9 +1,9 @@
  <!-- Left area  -->
  <div class="lg:w-[18%] bg-[#1D2026] vh-100">
      <div class="row">
-         <div class="col-10 offset-1 h-[10%]">
+         <div class="col-10 offset-1 h-[8%]">
              <div class="row">
-                 <img src="logo/logoo.png" alt="" width="100px" height="50px">
+                 <img src="logo/logoo.png" alt="" width="100px" height="45px">
              </div>
          </div>
          <div class="col-10 offset-1 h-[31rem]">
@@ -11,17 +11,26 @@
                  <div onclick="changeBgColor('adminDashboard');"  id="adminDashboard" class="text-start  p-[13px] rounded-1 hover:cursor-pointer item">
                      <i class="bi bi-columns-gap mx-4 text-lg text-white"></i><span class="text-white">Dashboard</span>
                  </div>
-                 <div  onclick="changeBgColor('adminManageUsers');" id="adminManageUsers" class="text-start  mt-3 p-[13px] rounded-1 hover:cursor-pointer item">
+                 <div  onclick="changeBgColor('adminManageUsers');" id="adminManageUsers" class="text-start  mt-2 p-[13px] rounded-1 hover:cursor-pointer item">
                      <i class="bi bi-people mx-4 text-lg text-white"></i><span class="text-white">Manage Users</span>
                  </div>
-                 <div onclick="changeBgColor('adminManageProducts');" id="adminManageProducts" class="text-start  mt-3 p-[13px] rounded-1 hover:cursor-pointer item">
+                 <div onclick="changeBgColor('adminManageProducts');" id="adminManageProducts" class="text-start  mt-2 p-[13px] rounded-1 hover:cursor-pointer item">
                      <i class="bi bi-box-seam mx-4 text-lg text-white"></i><span class="text-white">Manage Products</span>
                  </div>
-                 <div onclick="changeBgColor('adminOrders');" id="adminOrders" class="text-start  mt-3 p-[13px] rounded-1 hover:cursor-pointer item">
+                 <div onclick="changeBgColor('adminOrders');" id="adminOrders" class="text-start  mt-2 p-[13px] rounded-1 hover:cursor-pointer item">
                      <i class="bi bi-list-task mx-4 text-lg text-white"></i><span class="text-white">Orders</span>
                  </div>
-                 <div onclick="changeBgColor('adminPromotions');" id="adminPromotions" class="text-start  mt-3 p-[13px] rounded-1 hover:cursor-pointer item">
+                 <div onclick="changeBgColor('adminPromotions');" id="adminPromotions" class="text-start  mt-2 p-[13px] rounded-1 hover:cursor-pointer item">
                      <i class="bi bi-megaphone mx-4 text-lg text-white"></i><span class="text-white">Promotions</span>
+                 </div>
+                 <div onclick="changeBgColor('adminPromotions');" id="adminPromotions" class="text-start  mt-2 p-[13px] rounded-1 hover:cursor-pointer item">
+                     <i class="bi bi-tags mx-4 text-lg text-white"></i><span class="text-white">Categories</span>
+                 </div>
+                 <div onclick="changeBgColor('adminPromotions');" id="adminPromotions" class="text-start  mt-2 p-[13px] rounded-1 hover:cursor-pointer item">
+                     <i class="bi bi-app-indicator mx-4 text-lg text-white"></i><span class="text-white">Brands</span>
+                 </div>
+                 <div onclick="changeBgColor('adminPromotions');" id="adminPromotions" class="text-start  mt-2 p-[13px] rounded-1 hover:cursor-pointer item">
+                     <i class="bi bi-collection mx-4 text-lg text-white"></i><span class="text-white">Models</span>
                  </div>
              </div>
          </div>
