670e0f1d316920ea0ccd7f82d7130d5d5e007e92
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-04-14 22:07:30 +0530

    update adminDashboard

--- adminDashboard.php
+++ adminDashboard.php
@@ -209,9 +209,9 @@ class DashboardTemplete
             <script type="text/javascript">
                 jQuery(function($) {
                     var data1 = [12, 3, 4, 2, 12, 3, 4, 17, 22, 34, 54, 67];
-                    var data2 = [3, 9, 12, 14, 22, 32, 45, 12, 67, 45, 55, 7];
-                    var data3 = [23, 19, 11, 134, 242, 352, 435, 22, 637, 445, 555, 57];
-                    var data4 = [13, 19, 112, 114, 212, 332, 435, 132, 67, 45, 55, 7];
+                    // var data2 = [3, 9, 12, 14, 22, 32, 45, 12, 67, 45, 55, 7];
+                    // var data3 = [23, 19, 11, 134, 242, 352, 435, 22, 637, 445, 555, 57];
+                    // var data4 = [13, 19, 112, 114, 212, 332, 435, 132, 67, 45, 55, 7];
 
                     $("#chart1").shieldChart({
                         exportOptions: {
@@ -233,7 +233,6 @@ class DashboardTemplete
             <script type="text/javascript">
                 jQuery(function($) {
                     var data1 = [12, 3, 4, 2];
-                    var data2 = [3, 9, 12, 14];
 
                     $(function() {
                         $("#chart2").shieldChart({
