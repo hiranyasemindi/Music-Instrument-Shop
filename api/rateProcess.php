bd72d024f9e31db1e6dde62f032bdbd470ed0791
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-05-19 22:38:42 +0530

    update rate process

--- api/rateProcess.php
+++ api/rateProcess.php
@@ -28,10 +28,18 @@ class Process
     {
         if (isset($_GET["id"]) && isset($_GET["value"])) {
             if (isset($_SESSION["user"])) {
+                $email = $_SESSION["user"]["email"];
                 $product_id = $_GET["id"];
+                $value = $_GET["value"];
                 $product = $this->getProductById($product_id);
                 if ($product) {
-                    $this->updateProductRating($product_id, $_GET["value"]);
+                    $addedStatus = $this->getAddedRating($email, $product_id);
+                    if ($addedStatus) {
+                        $this->updateRating($email, $product_id, $value);
+                    } else {
+                        $this->addRating($email, $product_id, $value);
+                    }
+                    $this->calculateRating($product_id);
                     $this->responseObj->msg = "Success.";
                     $this->sendResponse();
                 } else {
@@ -46,6 +54,41 @@ class Process
             $this->responseObj->error = "Request parameters are empty.";
             $this->sendResponse(400);
         }
+    }
+
+    private function calculateRating($product_id)
+    {
+        $allRatings = $this->getRatingForProduct($product_id);
+        $rateSum = 0;
+        $count = 0;
+        foreach ($allRatings as $rating) {
+            $rateSum = $rateSum + (int)$rating["rating"];
+            $count++;
+        }
+        $avgRating = $rateSum / $count;
+        $this->updateProductRating($product_id, $avgRating);
+    }
+
+    private function getRatingForProduct($product_id)
+    {
+        $result = $this->search("SELECT * FROM `ratings` WHERE `ratings`.`product_id`='" . $product_id . "'");
+        return $result->num_rows > 0 ? $result : null;
+    }
+
+    private function addRating($email, $product, $rating)
+    {
+        $this->iud("INSERT INTO `ratings` (`user_email`,`product_id`,`rating`) VALUES ('" . $email . "','" . $product . "','" . $rating . "')");
+    }
+
+    private function updateRating($email, $product, $rating)
+    {
+        $this->iud("UPDATE `ratings` SET `rating`='" . $rating . "' WHERE `product_id`='" . $product . "' AND `user_email`='" . $email . "' ");
+    }
+
+    private function getAddedRating($email, $product)
+    {
+        $result = $this->search("SELECT * FROM `ratings` WHERE `product_id`='" . $product . "' AND `user_email`='" . $email . "'");
+        return $result->num_rows > 0 ? $result->fetch_assoc() : null;
     }
 
     private function updateProductRating($pid, $rating)
