d39f914daae30c4183e8e7c61e5a778656b1849a
Hiranya Semindi <hiranyagunawardhane@gmail.com>
2024-06-02 22:44:50 +0530

    aboutus responsive done

--- aboutUs.php
+++ aboutUs.php
@@ -28,14 +28,14 @@
                         <div class="bg-[#AD1212] my-2 h-[2px] w-[60%] d-block d-lg-none ml-[2%] rounded"></div>
                     </div>
 
-                    <div class="col-12">
+                    <div class="col-10 col-lg-12 offset-1 offset-lg-0">
                         <div class="row">
                             <div class="col-lg-7 col-12 ms-lg-4 ms-0 flex justify-end">
-                                <img src="assets/img/image 21.png" alt="" width="800px" height="600px">
+                                <img src="assets/img/image 21.png" alt="" width="952px" height="600px">
                             </div>
                             <div class="col-lg-4 col-12 ms-lg-3 ms-0">
-                                <img src="assets/img/image 22.png" class="mt-4 mt-lg-0 pt-3 pt-lg-0" alt="" width="400px" height="300px">
-                                <img src="assets/img/image 23.png" class="mt-4 pt-3" width="400px" height="300px">
+                                <img src="assets/img/image 22.png" class="mt-4 mt-lg-0 pt-3 pt-lg-0" alt="" width="550px" height="300px">
+                                <img src="assets/img/image 23.png" class="mt-4 pt-3" width="550px" height="300px">
                             </div>
                         </div>
                     </div>
@@ -43,7 +43,7 @@
                     <div class="col-lg-10 offset-lg-1 col-12 offset-0">
                         <div class="row">
                             <p class="pt-3 mt-4 text-center font-semibold text-3xl">Who We Are</p>
-                            <p class="text-lg p-5 text-[#999b9e]">Welcome to Sonority Music Shop, where passion for music meets exceptional craftsmanship.
+                            <p style="text-align: justify;" class="text-lg p-5 text-[#999b9e]">Welcome to Sonority Music Shop, where passion for music meets exceptional craftsmanship.
                                 As avid musicians ourselves, we understand the profound connection between an artist and their instrument. At Sonority Music Shop,
                                 we curate a diverse range of high-quality musical instruments, catering to both seasoned professionals and aspiring enthusiasts alike.
                                 </br></br>
@@ -65,23 +65,23 @@
 
                     <div class="col-12">
                         <div class="row">
-                            <div class="bg-white col-lg-4 col-12 flex items-center justify-center p-3 w-[500px] ">
+                            <div class="text-center col-12  p-3">
                                 <p class="text-[#AD1212] text-center font-semibold text-2xl"> Organization Vision & Mission</p>
                             </div>
-                            <div class="col-lg-8 col-12">
-                                <img src="assets/img/image 24.png" alt="" height="200px">
+                            <div class="flex mt-2 items-center justify-center col-10 offset-1 offset-lg-0 col-lg-12 ">
+                                <img src="assets/img/image 24.png" alt=""  height="200px" width="1550rem" >
                             </div>
 
                             <p class="text-center mt-5 font-semibold text-xl">Vision</p>
                             <div class="col-10 offset-1">
-                                <p class="text-lg px-5 mt-3 text-[#999b9e]">"To be the premier destination for musicians of all levels,
+                                <p style="text-align: justify;" class="text-lg px-lg-5 mt-3 text-[#999b9e]">"To be the premier destination for musicians of all levels,
                                     providing a diverse range of high-quality instruments and unparalleled customer service that inspires creativity,
                                     fosters community, and enriches lives through the power of music."</p>
                             </div>
 
                             <p class="text-center mt-5 font-semibold text-xl">Mission</p>
                             <div class="col-10 offset-1">
-                                <p class="text-lg px-5 mt-3 text-[#999b9e]">"At [Shop Name], our mission is to empower musicians by offering a curated
+                                <p style="text-align: justify;" class="text-lg px-lg-5 mt-3 text-[#999b9e]">"At [Shop Name], our mission is to empower musicians by offering a curated
                                     selection of top-tier instruments, personalized guidance, and expert services. We strive to create a welcoming
                                     environment where musicians of all backgrounds and skill levels feel valued and supported on their musical
                                     journey. Through our commitment to excellence, integrity, and passion for music, we aim to exceed expectations,
@@ -97,7 +97,7 @@
                             <div class="col-10 offset-1">
                                 <div class="row">
 
-                                    <div class="col-lg-3 col-12 mt-5">
+                                    <div class="col-lg-2 col-12 mt-5">
                                         <div class="row flex items-center justify-center">
                                             <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets//img/p1.jpg');">
                                             </div>
@@ -105,7 +105,7 @@
                                             <p class="text-center text-[#999b9e]">Director</p>
                                         </div>
                                     </div>
-                                    <div class="col-lg-3 col-12 mt-5">
+                                    <div class="col-lg-2 col-12 mt-5">
                                         <div class="row flex items-center justify-center">
                                             <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p2.jpg');">
                                             </div>
@@ -113,7 +113,7 @@
                                             <p class="text-center text-[#999b9e]">Director</p>
                                         </div>
                                     </div>
-                                    <div class="col-lg-3 col-12 mt-5">
+                                    <div class="col-lg-2 col-12 mt-5">
                                         <div class="row flex items-center justify-center">
                                             <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p3.jpg');">
                                             </div>
@@ -121,7 +121,23 @@
                                             <p class="text-center text-[#999b9e]">Director</p>
                                         </div>
                                     </div>
-                                    <div class="col-lg-3 col-12 mt-5">
+                                    <div class="col-lg-2 col-12 mt-5">
+                                        <div class="row flex items-center justify-center">
+                                            <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p4.jpg');">
+                                            </div>
+                                            <p class="text-center mt-2 font-semibold">Sandrew Mathew</p>
+                                            <p class="text-center text-[#999b9e]">Director</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-12 mt-5">
+                                        <div class="row flex items-center justify-center">
+                                            <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p4.jpg');">
+                                            </div>
+                                            <p class="text-center mt-2 font-semibold">Sandrew Mathew</p>
+                                            <p class="text-center text-[#999b9e]">Director</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-12 mt-5">
                                         <div class="row flex items-center justify-center">
                                             <div class="w-[160px] h-[160px] col-12 rounded-circle bg-cover" style="background-image: url('assets/img/p4.jpg');">
                                             </div>
@@ -137,7 +153,7 @@
                     <div class="col-lg-10 offset-lg-1 col-12 offset-0">
                         <div class="row">
                             <p class="text-center mt-5 pt-5 font-semibold text-3xl">History of Us</p>
-                            <p class="text-lg px-5 mt-3 text-[#999b9e]">"Established in 2001, Sonority Music Shop has been a cornerstone of the music community, proudly serving musicians
+                            <p style="text-align: justify;" class="text-lg px-5 mt-3 text-[#999b9e]">"Established in 2001, Sonority Music Shop has been a cornerstone of the music community, proudly serving musicians
                                 and enthusiasts alike for 23 years. Our journey began with a simple yet powerful vision: to create
                                 a haven where musicians could find exceptional instruments, expert guidance, and a supportive community to nurture
                                 their passion for music.
